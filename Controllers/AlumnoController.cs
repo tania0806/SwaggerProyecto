2d068e388b8d16c95b11164b33165f1c309491a4
tania0806 <13347@utl.edu.mx>
2024-09-13 20:27:08 -0600

    .

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -1,136 +1,136 @@
-using System;
-using Microsoft.AspNetCore.Mvc;
-using reportesApi.Services;
-using reportesApi.Utilities;
-using Microsoft.AspNetCore.Authorization;
-using reportesApi.Models;
-using Microsoft.Extensions.Logging;
-using System.Net;
-using reportesApi.Helpers;
-using Newtonsoft.Json;
-using System.IO;
-using OfficeOpenXml;
-using OfficeOpenXml.Style;
-using Microsoft.AspNetCore.Hosting;
-using reportesApi.Models.Compras;
+// using System;
+// using Microsoft.AspNetCore.Mvc;
+// using reportesApi.Services;
+// using reportesApi.Utilities;
+// using Microsoft.AspNetCore.Authorization;
+// using reportesApi.Models;
+// using Microsoft.Extensions.Logging;
+// using System.Net;
+// using reportesApi.Helpers;
+// using Newtonsoft.Json;
+// using System.IO;
+// using OfficeOpenXml;
+// using OfficeOpenXml.Style;
+// using Microsoft.AspNetCore.Hosting;
+// using reportesApi.Models.Compras;
 
-namespace reportesApi.Controllers
-{
+// namespace reportesApi.Controllers
+// {
    
-    [Route("api")]
-    public class AlumnoController: ControllerBase
-    {
+//     [Route("api")]
+//     public class AlumnoController: ControllerBase
+//     {
    
-        private readonly AlumnoService _AlumnoService;
-        private readonly ILogger<AlumnoController> _logger;
+//         private readonly AlumnoService _AlumnoService;
+//         private readonly ILogger<AlumnoController> _logger;
   
-        private readonly IJwtAuthenticationService _authService;
-        private readonly IWebHostEnvironment _hostingEnvironment;
+//         private readonly IJwtAuthenticationService _authService;
+//         private readonly IWebHostEnvironment _hostingEnvironment;
         
 
-        Encrypt enc = new Encrypt();
+//         Encrypt enc = new Encrypt();
 
-        public AlumnoController(AlumnoService AlumnoService, ILogger<AlumnoController> logger, IJwtAuthenticationService authService) {
-            _AlumnoService = AlumnoService;
-            _logger = logger;
+//         public AlumnoController(AlumnoService AlumnoService, ILogger<AlumnoController> logger, IJwtAuthenticationService authService) {
+//             _AlumnoService = AlumnoService;
+//             _logger = logger;
        
-            _authService = authService;
-            // Configura la ruta base donde se almacenan los archivos.
-            // Asegúrate de ajustar la ruta según tu estructura de directorios.
+//             _authService = authService;
+//             // Configura la ruta base donde se almacenan los archivos.
+//             // Asegúrate de ajustar la ruta según tu estructura de directorios.
 
             
             
-        }
+//         }
 
 
-        [HttpPost("InsertAlumno")]
-        public IActionResult InsertAlumnos([FromBody] InsertAlumnoModel req )
-        {
-            var objectResponse = Helper.GetStructResponse();
-            try
-            {
-                objectResponse.StatusCode = (int)HttpStatusCode.OK;
-                objectResponse.success = true;
-                objectResponse.message = _AlumnoService.InsertAlumno(req);
+//         [HttpPost("InsertAlumno")]
+//         public IActionResult InsertAlumnos([FromBody] InsertAlumnoModel req )
+//         {
+//             var objectResponse = Helper.GetStructResponse();
+//             try
+//             {
+//                 objectResponse.StatusCode = (int)HttpStatusCode.OK;
+//                 objectResponse.success = true;
+//                 objectResponse.message = _AlumnoService.InsertAlumno(req);
 
-            }
+//             }
 
-            catch (System.Exception ex)
-            {
-                objectResponse.message = ex.Message;
-            }
+//             catch (System.Exception ex)
+//             {
+//                 objectResponse.message = ex.Message;
+//             }
 
-            return new JsonResult(objectResponse);
-        }
+//             return new JsonResult(objectResponse);
+//         }
 
-        [HttpGet("GetAlumnos")]
-        public IActionResult GetAlumnos()
-        {
-            var objectResponse = Helper.GetStructResponse();
-            var resultado = _AlumnoService.GetAlumnos();
+//         [HttpGet("GetAlumnos")]
+//         public IActionResult GetAlumnos()
+//         {
+//             var objectResponse = Helper.GetStructResponse();
+//             var resultado = _AlumnoService.GetAlumnos();
 
-            try
-            {
-                objectResponse.StatusCode = (int)HttpStatusCode.OK;
-                objectResponse.success = true;
-                objectResponse.message = "data cargado con exito";
+//             try
+//             {
+//                 objectResponse.StatusCode = (int)HttpStatusCode.OK;
+//                 objectResponse.success = true;
+//                 objectResponse.message = "data cargado con exito";
 
 
-                // Llamando a la función y recibiendo los dos valores.
+//                 // Llamando a la función y recibiendo los dos valores.
                 
-                 objectResponse.response = resultado;
-            }
+//                  objectResponse.response = resultado;
+//             }
 
-            catch (System.Exception ex)
-            {
-                objectResponse.message = ex.Message;
-            }
+//             catch (System.Exception ex)
+//             {
+//                 objectResponse.message = ex.Message;
+//             }
 
-            return new JsonResult(objectResponse);
-        }
+//             return new JsonResult(objectResponse);
+//         }
 
-        [HttpPut("UpdateAlumno")]
-        public IActionResult UpdateAlumnos([FromBody] UpdateAlumnoModel req )
-        {
-            var objectResponse = Helper.GetStructResponse();
-            try
-            {
-                objectResponse.StatusCode = (int)HttpStatusCode.OK;
-                objectResponse.success = true;
-                objectResponse.message = _AlumnoService.UpdateAlumno(req);
+//         [HttpPut("UpdateAlumno")]
+//         public IActionResult UpdateAlumnos([FromBody] UpdateAlumnoModel req )
+//         {
+//             var objectResponse = Helper.GetStructResponse();
+//             try
+//             {
+//                 objectResponse.StatusCode = (int)HttpStatusCode.OK;
+//                 objectResponse.success = true;
+//                 objectResponse.message = _AlumnoService.UpdateAlumno(req);
 
-                ;
+//                 ;
 
-            }
+//             }
 
-            catch (System.Exception ex)
-            {
-                objectResponse.message = ex.Message;
-            }
+//             catch (System.Exception ex)
+//             {
+//                 objectResponse.message = ex.Message;
+//             }
 
-            return new JsonResult(objectResponse);
-        }
+//             return new JsonResult(objectResponse);
+//         }
 
-        [HttpDelete("DeleteAlumno/{id}")]
-        public IActionResult DeleteAlumno([FromRoute] int id )
-        {
-            var objectResponse = Helper.GetStructResponse();
-            try
-            {
-                objectResponse.StatusCode = (int)HttpStatusCode.OK;
-                objectResponse.success = true;
-                objectResponse.message = "data cargado con exito";
+//         [HttpDelete("DeleteAlumno/{id}")]
+//         public IActionResult DeleteAlumno([FromRoute] int id )
+//         {
+//             var objectResponse = Helper.GetStructResponse();
+//             try
+//             {
+//                 objectResponse.StatusCode = (int)HttpStatusCode.OK;
+//                 objectResponse.success = true;
+//                 objectResponse.message = "data cargado con exito";
 
-                _AlumnoService.DeleteAlumno(id);
+//                 _AlumnoService.DeleteAlumno(id);
 
-            }
+//             }
 
-            catch (System.Exception ex)
-            {
-                objectResponse.message = ex.Message;
-            }
+//             catch (System.Exception ex)
+//             {
+//                 objectResponse.message = ex.Message;
+//             }
 
-            return new JsonResult(objectResponse);
-        }
-    }
-}
+//             return new JsonResult(objectResponse);
+//         }
+//     }
+// }
