eee5bafc36ffb345e6773b4f0470671389206c38
AnthonyBuenoG <anthony.eb44@gmail.com>
2024-09-16 01:17:55 -0600

    Update RegistroModel.cs

--- Models/RegistroModel.cs
+++ Models/RegistroModel.cs
@@ -3,3 +3,8 @@ public class RegistroModel
     public string Correo { get; set; }
     public string Contraseña { get; set; }
 }
+public class RegistroInsertModel 
+    {
+        public string Correo { get; set; }
+        public string Contraseña { get; set; }
+    }
